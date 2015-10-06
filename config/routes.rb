9c059acd90a284499b819c4227a88587725fc7da
Gundars Ostrovskis <gundarso@gmail.com>
2015-10-06 12:04:17 +0200

    controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+resources :shops do
+  resources :sales
+end
+
+resources :customers
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
