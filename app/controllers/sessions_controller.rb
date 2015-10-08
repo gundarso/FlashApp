23a0a338fc33d5c2ca670a51baa8c78bf1758970
Gundars Ostrovskis <gundarso@gmail.com>
2015-10-08 12:13:01 +0200

    redirects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,20 @@
 class SessionsController < Devise::SessionsController
-  protected
 
-  def after_sign_in_path_for(resource)
-    resource.class == Shop ? shop_path(resource) : customer_path(resource)
+  def after_sign_in_path_for(customer)
+    if customer_signed_in? && customer.sign_in_count == 1
+      edit_customer_path(resource)
+    else
+      root_path
+    end
   end
+
+  def after_sign_in_path_for(shop)
+    if shop_signed_in? && shop.sign_in_count == 1
+      edit_shop_path(resource)
+    else
+      root_path
+    end
+  end
+
+
 end
