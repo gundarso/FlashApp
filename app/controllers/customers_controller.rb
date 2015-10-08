5c1c9a8f8157c6d80bcf04db8b5ad9409c2d1cb0
gderoq <guillermo.deroq@gmail.com>
2015-10-08 13:55:18 +0200

    ok

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,6 @@
 class CustomersController < ApplicationController
   before_action :authenticate_customer!
+  before_action :find_customer
 
   def show
   end
@@ -8,8 +9,27 @@ class CustomersController < ApplicationController
   end
 
   def update
+    @customer.update(customer_params)
+    if @customer.save
+      redirect_to customer_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @customer.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:username, :password)
+  end
+
+  def find_customer
+    @customer = Customer.find(params[:id])
   end
 end
+
