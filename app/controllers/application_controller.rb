bca893c2ce5cd785ef86fc564fba1ecb5469be50
gderoq <guillermo.deroq@gmail.com>
2015-10-08 20:29:23 +0200

    ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,8 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def after_sign_in_path_for(resource)
-    resource.class == Shop ? shop_path(resource) : customer_path(resource)
-  end
+  #def after_sign_in_path_for(resource)
+    #resource.class == Shop ? shop_path(resource) : customer_path(resource)
+  #end
 
 end
