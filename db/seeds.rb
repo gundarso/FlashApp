81916eaff0a3553a7373f6f32e70984a36a5f010
Gundars Ostrovskis <gundarso@gmail.com>
2015-10-13 14:27:22 +0200

    filed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,235 @@ Category.create(name_en: 'Health & beauty')
 Category.create(name_en: 'House & home')
 Category.create(name_en: 'Jewellery & accessories')
 Category.create(name_en: 'Other categories')
+
+Shop.create({
+  name: 'Zara',
+  street_address: 'Toison d\'Or 25',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8365051,
+  longitude: 4.3552722,
+  phone: '02923498',
+  contact_email: 'info@zarabxl.es'
+  })
+Shop.create({
+  name: 'Mediamarkt',
+  street_address: 'Bahnhofstrasse 7',
+  city: 'Aachen',
+  postcode: '52064',
+  country: 'Germany',
+  latitude: 50.7700137,
+  longitude: 6.0895888,
+  phone: '0392923498',
+  contact_email: 'aachenzentrum@mediamarkt.de'
+  })
+Shop.create({
+  name: 'H&M',
+  street_address: 'Rue Neuve 27',
+  city: 'Brussels',
+  postcode: '1000',
+  country: 'Belgium',
+  latitude: 50.8514998,
+  longitude: 4.3526746,
+  phone: '0223498',
+  contact_email: 'info@hetm.be'
+  })
+Shop.create({
+  name: 'GAP',
+  street_address: 'Chaussee d\'Ixelles 387',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8278498,
+  longitude: 4.3692292,
+  phone: '0209874',
+  contact_email: 'ixelles@gap.be'
+  })
+Shop.create({
+  name: 'FNAC',
+  street_address: 'Toison d\'Or 36',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8362745,
+  longitude: 4.3552685,
+  phone: '02245798',
+  contact_email: 'ixelles@fnac.be'
+  })
+Shop.create({
+  name: 'Zara',
+  street_address: 'Toison d\'Or 25',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8365051,
+  longitude: 4.3552722,
+  phone: '02923498',
+  contact_email: 'info@zarabxl.es'
+  })
+Shop.create({
+  name: 'Zara',
+  street_address: 'Toison d\'Or 25',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8365051,
+  longitude: 4.3552722,
+  phone: '02923498',
+  contact_email: 'info@zarabxl.es'
+  })
+Shop.create({
+  name: 'Zara',
+  street_address: 'Toison d\'Or 25',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8365051,
+  longitude: 4.3552722,
+  phone: '02923498',
+  contact_email: 'info@zarabxl.es'
+  })
+Shop.create({
+  name: 'Zara',
+  street_address: 'Toison d\'Or 25',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8365051,
+  longitude: 4.3552722,
+  phone: '02923498',
+  contact_email: 'info@zarabxl.es'
+  })
+Shop.create({
+  name: 'Zara',
+  street_address: 'Toison d\'Or 25',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8365051,
+  longitude: 4.3552722,
+  phone: '02923498',
+  contact_email: 'info@zarabxl.es'
+  })
+Shop.create({
+  name: 'Zara',
+  street_address: 'Toison d\'Or 25',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8365051,
+  longitude: 4.3552722,
+  phone: '02923498',
+  contact_email: 'info@zarabxl.es'
+  })
+Shop.create({
+  name: 'Zara',
+  street_address: 'Toison d\'Or 25',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8365051,
+  longitude: 4.3552722,
+  phone: '02923498',
+  contact_email: 'info@zarabxl.es'
+  })
+Shop.create({
+  name: 'Zara',
+  street_address: 'Toison d\'Or 25',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8365051,
+  longitude: 4.3552722,
+  phone: '02923498',
+  contact_email: 'info@zarabxl.es'
+  })
+Shop.create({
+  name: 'Zara',
+  street_address: 'Toison d\'Or 25',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8365051,
+  longitude: 4.3552722,
+  phone: '02923498',
+  contact_email: 'info@zarabxl.es'
+  })
+Shop.create({
+  name: 'Zara',
+  street_address: 'Toison d\'Or 25',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8365051,
+  longitude: 4.3552722,
+  phone: '02923498',
+  contact_email: 'info@zarabxl.es'
+  })
+Shop.create({
+  name: 'Zara',
+  street_address: 'Toison d\'Or 25',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8365051,
+  longitude: 4.3552722,
+  phone: '02923498',
+  contact_email: 'info@zarabxl.es'
+  })
+Shop.create({
+  name: 'Zara',
+  street_address: 'Toison d\'Or 25',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8365051,
+  longitude: 4.3552722,
+  phone: '02923498',
+  contact_email: 'info@zarabxl.es'
+  })
+Shop.create({
+  name: 'Zara',
+  street_address: 'Toison d\'Or 25',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8365051,
+  longitude: 4.3552722,
+  phone: '02923498',
+  contact_email: 'info@zarabxl.es'
+  })
+Shop.create({
+  name: 'Zara',
+  street_address: 'Toison d\'Or 25',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8365051,
+  longitude: 4.3552722,
+  phone: '02923498',
+  contact_email: 'info@zarabxl.es'
+  })
+Shop.create({
+  name: 'Zara',
+  street_address: 'Toison d\'Or 25',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8365051,
+  longitude: 4.3552722,
+  phone: '02923498',
+  contact_email: 'info@zarabxl.es'
+  })
+Shop.create({
+  name: 'Zara',
+  street_address: 'Toison d\'Or 25',
+  city: 'Ixelles',
+  postcode: '1050',
+  country: 'Belgium',
+  latitude: 50.8365051,
+  longitude: 4.3552722,
+  phone: '02923498',
+  contact_email: 'info@zarabxl.es'
+  })
