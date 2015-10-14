4ed223f30cf0ceb2cca9c68c3a117ea2910d1880
Gundars Ostrovskis <gundarso@gmail.com>
2015-10-14 09:23:35 +0200

    map fixed to sales

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -9,9 +9,9 @@ class CustomersController < ApplicationController
 
     @sales = @sales.select { |sale| @shops.include?(sale.shop)}
     # Let's DYNAMICALLY build the markers for the view.
-    @markers = Gmaps4rails.build_markers(@shops.select(&:latitude)) do |shop, marker|
-      marker.lat shop.latitude
-      marker.lng shop.longitude
+    @markers = Gmaps4rails.build_markers(@sales) do |sale, marker| @shops.select(&:latitude)
+      marker.lat sale.shop.latitude
+      marker.lng sale.shop.longitude
     end
   end
 
@@ -54,5 +54,11 @@ class CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
 
+  def query
+    @search = Rule.search do
+    fulltext params[:search]
+    end
+    @rules = @search.results
+  end
 end
 
